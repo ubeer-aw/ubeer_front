43e4673021b2fe4854569e4d2bb1714b66988eb5
Arthur HEUDE <arthur.heude@laposte.net>
2023-03-03 09:59:12 +0100

    update appbar

--- src/app/views/components/appbar/Appbar.js
+++ src/app/views/components/appbar/Appbar.js
@@ -10,6 +10,9 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Leftdrawer from '../leftdrawer/Leftdrawer'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import styles from './Appbar.css';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const theme = createTheme({
@@ -32,6 +35,41 @@ const theme = createTheme({
   },
 });
 
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: '50px',
+  backgroundColor: '#EEEEEE',
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(5),
+    marginRight: theme.spacing(5),
+    width: '40%',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 1),
+    fontSize:'14px',
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    width: '100%',
+  },
+}));
+
+
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -48,11 +86,20 @@ export default function ButtonAppBar() {
             <Leftdrawer />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <h6 style={{display: 'inline-block', fontSize:'2vh' }}>U</h6><h6 style={{color: '#06C167', display: 'inline-block', fontSize:'2vh' }}>beer</h6>
+          <h6 style={{display: 'inline-block', fontSize:'4vh', margin:'0' }}>U</h6><h6 style={{color: '#06C167', display: 'inline-block', fontSize:'4vh', margin:'0' }}>beer</h6>
           </Typography>
           <ThemeProvider theme={theme}>
-            <Button disableElevation variant="contained" color="grey"  size="large" sx={{textTransform: 'none', borderRadius: '50px'}}><LocationOnIcon sx={{mr:1}}/> 16 Bd Général de Gaulle • Maintenant</Button>
-            <Button disableElevation variant="contained" color="dark"  size="large" sx={{textTransform: 'none', borderRadius: '50px'}}><ShoppingCartIcon sx={{mr:1}}/> panier</Button>
+            <Button disableElevation variant="contained" color="grey"  size="large" sx={{textTransform: 'none', borderRadius: '50px', fontSize:'13px'}}><LocationOnIcon sx={{mr:1}}/> 16 Bd Général de Gaulle • Maintenant</Button>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Brasserie, bière, etc."
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+            <Button disableElevation variant="contained" color="dark"  size="large" sx={{textTransform: 'none', borderRadius: '50px'}}><ShoppingCartIcon sx={{mr:1}}/> 0 paniers</Button>
           </ThemeProvider>
         </Toolbar>
       </AppBar>
